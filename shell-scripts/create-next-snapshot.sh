7e04ef50c5f9c557e7d8298da0f6b86f8338a1e2
Coding Speedup <coding.speedup@gmail.com>
2022-05-10 18:56:38 +0300

    fixed issues related to nexus snapshot upload

--- shell-scripts/create-next-snapshot.sh
+++ shell-scripts/create-next-snapshot.sh
@@ -2,5 +2,5 @@ cd "${BASH_SOURCE:-$0:h:h}" || exit
 echo Working in $(pwd)
 NEXT_VERSION=$(date +"%Y-%m-%d.%H")-SNAPSHOT
 echo Updating version to "$NEXT_VERSION"
-#mvn versions:set -DnewVersion="$NEXT_VERSION"
-#mvn versions:commit
+mvn versions:set -DnewVersion="$NEXT_VERSION"
+mvn versions:commit
--- shell-scripts/deploy-snapshots.sh
+++ shell-scripts/deploy-snapshots.sh
@@ -3,28 +3,6 @@ PROJECT_ROOT=$(pwd)
 echo Starting from "$PROJECT_ROOT"
 
 mvn clean
-
-cd "${PROJECT_ROOT}/execdoc-kb" || exit
-cd "${PROJECT_ROOT}/execdoc-core" || exit
-cd "${PROJECT_ROOT}/execdoc-apps" || exit
-
-cd "${PROJECT_ROOT}/execdoc-kb"
-echo Working in $(pwd)
 mvn source:jar deploy || exit
 
-cd "${PROJECT_ROOT}/execdoc-core"
-echo Working in $(pwd)
-mvn source:jar deploy || exit
-
-cd "${PROJECT_ROOT}/execdoc-apps"
-echo Working in $(pwd)
-mvn source:jar deploy || exit
-
-cd "${PROJECT_ROOT}/execdoc-spring"
-echo Working in $(pwd)
-mvn source:jar deploy || exit
-
-cd "${PROJECT_ROOT}/execdoc-jhipster"
-echo Working in $(pwd)
-mvn source:jar deploy || exit
 
